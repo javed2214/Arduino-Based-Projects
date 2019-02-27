b70c09853a046a6271a95108370d6195ec298a5e
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-02-27 20:50:53 +0530

    Add files via upload

---
+++ Buzzer.ino
@@ -0,0 +1,17 @@
+const int buzzer = 7; 
+
+
+void setup(){
+ 
+  pinMode(buzzer, OUTPUT); 
+
+}
+
+void loop(){
+ 
+  tone(buzzer, 700); 
+  delay(500);        
+  noTone(buzzer);     
+  delay(150);        
+  
+}
