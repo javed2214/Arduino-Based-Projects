e735a67f92ab31dc3721f08de5f7328e1c64b654
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-04-05 22:21:26 +0530

    Update and rename soilirrigation.ino to Soil-Irrigation.ino

---
+++ Soil-Irrigation.ino
@@ -0,0 +1,61 @@
+
+#include<Arduino.h>
+
+#define ENA 9
+#define In11 2
+#define In12 3
+
+int msensor=A1;
+int msvalue=0;
+int led=13;
+boolean flag=false;
+
+
+void setup() {
+
+  pinMode(ENA, OUTPUT);
+  pinMode(In11, OUTPUT);
+  pinMode(In12, OUTPUT);
+  pinMode(msensor,INPUT);
+  pinMode(led,OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void pumpON(){
+    
+  analogWrite(ENA, 255);
+  digitalWrite(In11, HIGH); 
+  digitalWrite(In12, LOW);
+}
+
+void pumpOFF(){
+
+   analogWrite(ENA,0);
+   digitalWrite(In11, HIGH); 
+   digitalWrite(In12, LOW);
+}
+
+void loop() {
+ 
+  msvalue=analogRead(msensor);
+  Serial.println(msvalue);
+
+  if(msvalue>500)
+  {
+      pumpON();
+      digitalWrite(led,HIGH);
+//    flag=true;
+      delay(1000);
+      
+  }
+       else
+       {
+          pumpOFF();
+          digitalWrite(led,LOW);
+//        flag=false;
+          delay(1000);
+       }
+          delay(10);
+
+}
